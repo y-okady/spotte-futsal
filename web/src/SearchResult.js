7794c8b16de716744189e3647346f9a523de30b6
Yuki Okada <y.okady.221@gmail.com>
2018-10-28 19:34:09 +0900

    #9 フットサルコート一覧をカードで表示

--- web/src/SearchResult.js
+++ web/src/SearchResult.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
+import './SearchResult.css';
+import { Card, CardActionArea, CardContent, Typography } from '@material-ui/core';
 
 class SearchResult extends Component {
-  renderCourts(courts) {
-    return (
-      <ul>
-        {courts.map((court, index) => {
-          return (
-            <li key={`court-${index}`}>
-              {court}
-            </li>
-          );
-        })}
-      </ul>
-    )
-  }
   render() {
     return (
       <div className="SearchResult">
-        <ul>
-          {this.props.spots.map((spot, index) => {
-            return (
-              <li key={`spot-${index}`}>
-                {spot.spot}
-                {this.renderCourts(spot.courts)}
-              </li>
-            );
-          })}
-        </ul>
+        {this.props.spots.map((spot, index) => {
+          return (
+            <Card key={`spot-${index}`} className="SearchResult-item">
+              <CardActionArea>
+                <CardContent>
+                  <Typography variant="title" gutterBottom>
+                    {spot.spot}
+                  </Typography>
+                  <Typography variant="body1">
+                    空きコート: {spot.courts.join(', ')}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
       </div>
     );
   }
