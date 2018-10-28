c10612f79c3cbb5cfc4c8610df2e52fc4b5f01de
Yuki Okada <y.okady.221@gmail.com>
2018-10-28 18:31:29 +0900

    #9 ヘッダー

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import SearchForm from './SearchForm';
 import SearchResult from './SearchResult';
 import axios from 'axios';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFutbol } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faFutbol)
 
 const SEARCH_API_URL = 'https://qxkhhtc4d0.execute-api.ap-northeast-1.amazonaws.com/dev/search';
 
@@ -30,8 +35,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          Spotte Futsal
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <FontAwesomeIcon icon="futbol" size="lg" />
         </header>
         <SearchForm onSubmit={condition => this.search(condition)}/>
         <SearchResult spots={this.state.spots} />
