0470073ed3a54bed0558e03ad0d77e288f86ed28
Yuki Okada <y.okady.221@gmail.com>
2018-10-31 00:40:54 +0900

    #12 PCの見た目調整

--- web/src/SearchResult.js
+++ web/src/SearchResult.js
@@ -7,21 +7,24 @@ class SearchResult extends Component {
       <div className="SearchResult">
         {this.props.spots.map((spot, index) => {
           return (
-            <div key={`spot-${index}`} className="SearchResult-item">
-              <h3 className="SearchResult-spot">
-                <a href={spot.url} target="_blank">{spot.spot}</a>
-              </h3>
-              <div className="SearchResult-courts">{spot.courts.join(', ')}</div>
-              <div className="SearchResult-booking">
-                <a href={spot.bookingUrl} target="_blank">コートを予約する</a>
-              </div>
-              <div className="SearchResult-map">
-              <iframe
-                width="600"
-                height="450"
-                frameBorder="0"
-                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyB1vbtwJx0IRSdjIH6LfnQrU1wxoLHIYwk&q=${spot.spot}`}
-                allowFullScreen />
+            <div key={`spot-${index}`} className="SearchResult-itemOuter">
+              <div className="SearchResult-item">
+                <h3 className="SearchResult-spot">
+                  <a href={spot.url} target="_blank" rel="noopener noreferrer">{spot.spot}</a>
+                </h3>
+                <div className="SearchResult-courts">{spot.courts.join(', ')}</div>
+                <div className="SearchResult-booking">
+                  <a href={spot.bookingUrl} target="_blank" rel="noopener noreferrer">コートを予約する</a>
+                </div>
+                <div className="SearchResult-map">
+                <iframe
+                  title="Google Maps"
+                  width="600"
+                  height="450"
+                  frameBorder="0"
+                  src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyB1vbtwJx0IRSdjIH6LfnQrU1wxoLHIYwk&q=${spot.spot}`}
+                  allowFullScreen />
+                </div>
               </div>
             </div>
           );
