11f3fc19075f533e7b13f31e221869ff91006773
Yuki Okada <y.okady.221@gmail.com>
2018-11-05 23:34:35 +0900

    #16 VLCMのフットサルコート追加

--- aws/spot.js
+++ aws/spot.js
@@ -24,6 +24,41 @@ const SPOTS = [
     'http://www.sportsoasis.co.jp/sh18/futsal/',
     VLCMCrawler, '00169', true
   ),
+  new Spot(
+    'FUT MESSE 海老江',
+    'http://www.fut-messe.com/ebie/',
+    VLCMCrawler, '00175'
+  ),
+  new Spot(
+    'FUT MESSE 大正',
+    'http://www.fut-messe.com/taisho/',
+    VLCMCrawler, '00053'
+  ),
+  new Spot(
+    'FUT MESSE 鶴見緑地',
+    'http://www.fut-messe.com/tsurumi/',
+    VLCMCrawler, '00162'
+  ),
+  new Spot(
+    'FUT MESSE 生野',
+    'http://www.fut-messe.com/ikuno/',
+    VLCMCrawler, '00065'
+  ),
+  new Spot(
+    'マグフットサルスミノエ',
+    'http://futsal.mags.co.jp/',
+    VLCMCrawler, '00052'
+  ),
+  new Spot(
+    'マグフットサルスタジアム',
+    'http://futsal.mags.co.jp/',
+    VLCMCrawler, '00051'
+  ),
+  new Spot(
+    'クーバー・フットボールパーク 大阪平野',
+    'http://www.coerver-footballpark.com/osaka-hirano/',
+    VLCMCrawler, '00007'
+  ),
 ];
 
 const SPOTS_MAP = Object.assign(...SPOTS.map(spot => ({[spot.spot]: spot})));
